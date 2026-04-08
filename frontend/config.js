364f33807bd03682b6cd77308230921c7a230f17
return-ibrahim <mohdibrahim2504@gmail.com>
2026-04-08 09:11:33 +0530

    Fix UI layout alignment and offline fetch error

--- frontend/config.js
+++ frontend/config.js
@@ -4,7 +4,7 @@
 let API_BASE_URL;
 
 // Check if we are running on a local computer
-if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
+if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1" || window.location.hostname === "") {
     
     // Local Flask Backend
     API_BASE_URL = "http://127.0.0.1:5000";
